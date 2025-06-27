0bac1d226f55fc55582d0ba7fd2b98b2eee3c33b
karpovv05 <karps5599@icloud.com>
2025-06-27 16:39:48 +0300

    change

--- main.py
+++ main.py
@@ -9,16 +9,15 @@ app = Flask(__name__)
 def index():
     return 'Flask работает!'
 
-@app.route('/echo', methods=['POST'])
+@app.route('/post', methods=['POST'])
 def echo_json():
     if not request.is_json:
         return jsonify({'error': 'Ожидался JSON'}), 400
     data = request.get_json()
     # Логируем все атрибуты
     logging.info(f"Получен JSON: {data}")
-    # Формируем строку с атрибутами и их значениями
-    attrs = '\n'.join([f"{k}: {v}" for k, v in data.items()])
-    return f"Получены атрибуты:\n{attrs}", 200
+
+    return f"Получены атрибуты:", 200
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=False)
+    app.run(host='0.0.0.0', port=5000, debug=True)
